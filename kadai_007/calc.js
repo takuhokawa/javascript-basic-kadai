4c06d198d4f38cebd51df2aa0471169229d64a8d
takuhokawa <taku.2263.db@gmail.com>
2025-03-16 10:11:47 +0900

    kadai_007をアップロード

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -4,11 +4,11 @@ if (num % 15 === 0) {
   console.log('3と5の倍数です');
 }
 
-else if (num % 3 === 0 && num % 15 !== 0 ) {
+else if (num % 3 === 0) {
   console.log('3の倍数です');
 }
 
-else if (num % 5 === 0 && num % 15 !== 0 ) {
+else if (num % 5 === 0) {
   console.log('5の倍数です');
 }
 
